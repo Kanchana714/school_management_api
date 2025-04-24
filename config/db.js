a2d6da21dad5937bf6f7e00f90e8248905f05657
kanchana714 <kanchanabalamurugan04@gmail.com>
2025-04-24 15:22:48 +0530

    update db

--- config/db.js
+++ config/db.js
@@ -1,17 +1,14 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to the database as id ' + connection.threadId);
-});
+module.exports = pool;
