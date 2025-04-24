b0ec335f6ebf92c8ff0060aed70ca6b8cc479c66
kanchana714 <kanchanabalamurugan04@gmail.com>
2025-04-24 11:13:17 +0530

    Initial commit : Express API with school routes

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const express = require('express');
 const app = express();
 const schoolRoutes = require('./routes/schoolRoutes');
 
+// Middleware to parse JSON requests
 app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+// Mount school routes under /api
 app.use('/api', schoolRoutes);
 
+// Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
